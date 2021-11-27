b6995f83de8c4b5df38001e96662c5b168cb4810
lukeg <luke.goboyan@gmail.com>
2021-11-27 12:53:25 +0100

    on a fait qques changements

--- classes.py
+++ classes.py
@@ -3,9 +3,10 @@ import assets
 
 
 class Button:
-    def __init__(self, x, y, text, resize_x=1, resize_y=1, ombre=False, on_click=lambda: print("unregistred button click")):
+    def __init__(self, x, y, text, flag, resize_x=1, resize_y=1, ombre=False, on_click=lambda: print("unregistred button click")):
         # on_click est une fonction elle s'execute lorsqu'un click est détecté (regardez les lambdas)
         self.on_click = on_click
+        self.flag = flag
         self.screen = None
         self.sprite_list = [pygame.transform.scale(assets.button1, (60 * resize_x, 30 * resize_y)),
                             pygame.transform.scale(assets.button2, (60 * resize_x, 30 * resize_y))]
@@ -29,13 +30,9 @@ class Button:
         """Verification des valeurs"""
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             self.sprite = self.sprite_list[1]
-            # ca ne marche pas d'itérer comme ca pck on get dejà les event dans le main, et si on get une autre fois on a rien de plus
-            # for event in pygame.event.get():
-            #     if event.type == pygame.MOUSEBUTTONDOWN:
-            #         print("fin du monde")
-            #     J'ai trouvé une autre manière de le faire:
             if interfaceClicked:
                 self.on_click()
+                self.clicked = True
             if pygame.mouse.get_pressed()[0]:
                 offset = 5
         else:
@@ -52,8 +49,10 @@ class Button:
 
 
 class Interface:
-    def __init__(self, button_list, state, screen, background_color=(-1, -1, -1)):
+    def __init__(self, button_list, flag, state, screen, background_sprite):
         self.button_list = button_list
+        self.flag = flag
+        self.flagchange = -1
         self.active = state
         self.clicked = False
         self.screen = screen
@@ -61,19 +60,23 @@ class Interface:
             button.screen = screen
         #     les éléments sont stockés dans une liste de tuple (surface, pos)
         self.elements = []
-        self.background_color = background_color
+        self.background_sprite = background_sprite
 
     def update(self):
         # Si le background n'a pas été défini, on n'affiche pas de background
-        if self.background_color != (-1, -1, -1):
-            self.screen.fill(self.background_color)
+        #if self.background_color != (-1, -1, -1):
+            #self.screen.fill(self.background_color)
             # On affiche chaque éléments
+
+        self.screen.blit(self.background_sprite, (0, 0))
 
         for element in self.elements:
             self.screen.blit(element[0], element[1])
 
         for button in self.button_list:
             button.update(self.clicked)
+            if button.clicked:
+                self.flagchange = button.flag
 
         self.clicked = False
 
@@ -87,23 +90,37 @@ class Interface:
 
 
 class Aliments:
-    """Carectiristique de chaque aliments"""
+    """Carectèristique de chaque aliments"""
 
-    def __init__(self, name, image_empty, image_grown, image_end):
-        self.nom = name
-        self.image_vide = image_empty
-        self.image_pousse = image_grown
-        self.image_fin = image_end
+    def __init__(self, name, image_seed1, image_seed2, image_fertilizer, image_fertiliser_ground, image_natural, image_natural_ground, image_bag_natural, image_bag_fertilizer):
+        self.name = name
+        self.image_seed1 = image_seed1
+        self.image_seed2 = image_seed2
+        self.image_fertilizer = image_fertilizer
+        self.image_fertilizer_ground = image_fertiliser_ground
+        self.image_natural = image_natural
+        self.image_natural_ground = image_natural_ground
+        self.image_bag_natural = image_bag_natural
+        self.image_bag_fertilizer = image_bag_fertilizer
 
 
 class Case:
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, seed=None,):
         self.x = x
         self.y = y
-        self.image = image
+        self.seed = seed
 
     def get_pose(self):
         return self.x, self.y
+
+    def inventory(self):
+        return self.seed
+
+    def set_seed(self, seed):
+        self.seed = seed
+
+    def get_seed(self):
+        return self.seed
 
 # class MenuScreen:
 #     def __init__(self, bestScore):
@@ -114,3 +131,16 @@ class Case:
 #         screen.fill((47, 129, 54))
 #         screen.blit(assets.title_font.render("Farmer Simulator", False, (237, 226, 108)), (100, 100))
 #         self.quitButton.update()
+
+
+#Création des cases
+list_cases = []
+for i in range(20):
+    for j in range(12):
+        list_cases.append(Case(360+i*30, 120+j*30))
+
+Carotte = Aliments("carotte", assets.carrots_seed1, assets.carrots_seed2, assets.carrots_fertilizer, assets.carrots_fertilizer_ground, assets.carrots_natural, assets.carrots_natural_ground, assets.carrots_bag_natural, assets.carrots_bag_fertilizer)
+Corn = Aliments("corn", assets.corn_seed1, assets.corn_seed2, assets.corn_fertilizer, assets.corn_fertilizer_ground, assets.corn_natural, assets.corn_natural_ground, assets.corn_bag_natural, assets.corn_bag_fertilizer)
+Potato = Aliments("potato", assets.potato_seed1, assets.potato_seed2, assets.potato_fertilizer, assets.potato_fertilizer_ground, assets.potato_natural, assets.potato_natural_ground, assets.potato_bag_natural, assets.potato_bag_fertilizer)
+Tomato = Aliments("tomato", assets.tomato_seed1, assets.tomato_seed2, assets.tomato_fertilizer, assets.tomato_fertilizer_ground, assets.tomato_natural, assets.tomato_natural_ground, assets.tomato_bag_natural, assets.tomato_bag_fertilizer)
+Turnip = Aliments("turnip", assets.turnip_seed1, assets.turnip_seed2, assets.turnip_fertilizer, assets.turnip_fertilizer_ground, assets.turnip_natural, assets.turnip_natural_ground, assets.turnip_bag_natural, assets.turnip_bag_fertilizer)
--- main.py
+++ main.py
@@ -14,36 +14,62 @@ def game_quit():
     pygame.quit()
     sys.exit()
 
-
 # Importé après avoir init et ouvert le screen sinon il y a une erreur
 import assets
 import classes
 
+
+def changeInterface(interfacelist, flag):
+    for interface in interfacelist:
+        if interface.flag == flag:
+            interface.active = True
+        else:
+            interface.active = False
+
+def change_food(aliment):
+    """fonction qui place les images correspondant a l'aliment selectionné """
+    for elt in classes.list_cases:
+        elt.set_seed(aliment.image_seed1)
+        screen.blit(elt.get_seed(), elt.get_pose())
+
+
 # Liste de boutons
-buttons = [
-    classes.Button(screen.get_width() // 4 - 30, screen.get_height() // 4 * 3, "Play", 2, 2, True),
-    classes.Button(screen.get_width() // 4 * 3 - 30, screen.get_height() // 4 * 3, "Quit", 2, 2, True,
+ttle_screen_buttons = [
+    classes.Button(screen.get_width() // 4 - 30, screen.get_height() // 4 * 3, "Play", 1, 2, 2, True),
+    classes.Button(screen.get_width() // 4 * 3 - 30, screen.get_height() // 4 * 3, "Quit", (-1), 2, 2, True,
                    lambda: game_quit())
 ]
 
-# On crée le fond en mettant de la grass random
-background = pygame.surface.Surface(screen.get_size())
-background.set_colorkey((0, 0, 0))
-for x in range(screen.get_width() // 30 + 1):
-    for y in range(screen.get_height() // 27 + 1):
-        r = random.random()
-        if r < .24:
-            background.blit(assets.grass, (x * 30, y * 30))
-        elif r < .30:
-            background.blit(assets.short_grass, (x * 30, y * 30))
-        elif r < .31:
-            background.blit(assets.haybale, (x * 30, y * 30))
+main_screen_buttons = [
+    classes.Button(screen.get_width() // 100, screen.get_height() // 50, "Sav&Quit", 0, 2, 2, True)
+]
 
-interface = classes.Interface(buttons, True, screen,
-                              background_color=(47, 129, 54))
-interface.addElement((background, (0, 0)))
-interface.addElement((assets.title_font.render("Farmer Simulator", False, (219, 215, 101)),
+fields_buttons = []
+
+#Liste des interfaces
+title_screen = classes.Interface(ttle_screen_buttons, 0, True, screen, assets.title_background)
+                                 #background_color=(47, 129, 54))
+
+# On crée le fond en mettant de la grass random
+#title_background = pygame.surface.Surface(screen.get_size())
+#title_background.set_colorkey((0, 0, 0))
+#for x in range(screen.get_width() // 30 + 1):
+#    for y in range(screen.get_height() // 27 + 1):
+#        r = random.random()
+#        if r < .24:
+#            title_background.blit(assets.grass, (x * 30, y * 30))
+#        elif r < .30:
+#            title_background.blit(assets.short_grass, (x * 30, y * 30))
+#        elif r < .31:
+#            title_background.blit(assets.haybale, (x * 30, y * 30))
+
+#title_screen.addElement((title_background, (0, 0)))
+title_screen.addElement((assets.title_font.render("Farmer Simulator", False, (219, 215, 101)),
                       (screen.get_width() // 2 - 400, 50, 100, 100)))
+
+
+main_screen = classes.Interface([main_screen_buttons], 1, False, screen, assets.field_background)
+interface_list = [title_screen, main_screen]
 
 while True:
     for event in pygame.event.get():
@@ -51,10 +77,16 @@ while True:
             game_quit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            interface.click()
+            for interface in interface_list:
+                if interface.active:
+                    interface.click()
+                if interface.flagchange>=0:
+                    changeInterface(interface_list, interface.flagchange)
+
 
     # J'ai créé une methode dans la class Interface qui update tout ses boutons
-    interface.update()
-
     pygame.display.update()
     pygame.time.delay(10)
+
+
+
